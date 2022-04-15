0ecb70cd84f47ecf50b834e94ddec46c3cb3fa2e
nefanov <efanov@phystech.edu>
2022-04-15 17:54:00 +0300

    Create run_pipeline.sh

---
+++ materials/seminar11/run_pipeline.sh
@@ -0,0 +1,8 @@
+VISUALIZER=gprof2dot.py     
+if [ ! -f $VISUALIZER ]; then
+    wget https://raw.githubusercontent.com/jrfonseca/gprof2dot/master/gprof2dot.py
+fi
+
+g++ 1.cpp -o a.out
+valgrind --tool=callgrind ./a.out
+python3 gprof2dot.py -n0 -e0 $(find . -name callgrind.out* | tail -n 1) -f callgrind | dot  -Tpng -o $(find . -name callgrind.out* | tail -n 1)_pic.png
